d7e1842cc4914005ec9b01d66b5cb1963f709adb
Miguel Aroles <miguelaroles@gmail.com>
2022-07-06 12:23:34 +0200

    CREATE : Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
